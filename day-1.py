476a5152938ec30a76db2cc010ef303efed0afc2
YuvarajVempati <yuvarajvempati@gmail.com>
2025-01-21 15:13:33 +0530

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,37 @@
+print('Arithmatic  Operators')
+print(2+78)
+print(60-4)
+print(30*10)
+print(100/2)
+print(100%5)
+print(486+879)
+print(7989-3456)
+print(157*3)
+print(123/345)
+num1=123
+num2=345
+print(num1+num2)
+print(num1-num2)
+num2=5
+print(num1*num2)
+print(num1/num2)
+print(0**0)
+print('Length Functions')
+var1="345"
+print(var1)
+print(len(var1))
+print('var1')
+var2="  "
+print(len(var2))
+var3=""
+print(len(var3))
+print('Type')
+var4=34.7
+print(type(34.7))
+var5="python html css java script git github aws"
+print('srting index')
+print(len(var5))
+print(var5[2])# string indexing
+print(var5[-2])
+print('slicing')
+print(var5[0:5])#slising function
